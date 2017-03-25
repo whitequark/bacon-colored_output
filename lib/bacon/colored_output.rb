becdac2e28ba2a6ed21d0551757dce3b7405317a
Alexander Popov <alex.wayfer@gmail.com>
2017-03-25 13:07:31 +0300

    Make colored output universal (for any output style)

--- lib/bacon/colored_output.rb
+++ lib/bacon/colored_output.rb
@@ -1,15 +1,11 @@
 module Bacon
   module ColoredOutput
-    include SpecDoxOutput
-
-    def handle_requirement(description)
-      print spaces
-
+    def handle_requirement(*args)
       error = yield
 
       print error.empty? ? color("\e[32m") : color("\e[1;31m")
-      print "  - #{description}"
-      puts error.empty? ? color("\e[0m") : " [#{error}]#{color("\e[0m")}"
+      super
+      print color("\e[0m")
     end
 
     def color(escape_seq)
